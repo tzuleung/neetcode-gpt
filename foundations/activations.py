1cdfa32dbf960c97e075c72917ef7a21a00fa9ad
Tzu Feng <32727072+tzuleung@users.noreply.github.com>
2026-04-19 10:47:02 -0700

    Implement Activation functions (Math Foundations 2/6)

---
+++ foundations/activations.py
@@ -0,0 +1,21 @@
+import numpy as np
+from numpy.typing import NDArray
+
+'''
+sigmoid: logistic: 1 / (1+exp(-x))
+relu: max(0, x) "all neg becomes 0".
+'''
+
+
+class Solution:
+    
+    def sigmoid(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        # z is a 1D NumPy array
+        # Formula: 1 / (1 + e^(-z))
+        # return np.round(your_answer, 5)
+        return np.round(1 / (1 + np.exp(-z)), 5)
+
+    def relu(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        # z is a 1D NumPy array
+        # Formula: max(0, z) element-wise
+        return np.maximum(0,z)
